cdac0fb660e1d6cc3c345f73776634484c70826d
Sid Srinivas <siddharths2710@yahoo.com>
2023-03-11 20:37:07 -0700

    Retrieve events from Firestore DB

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+var db = null;
+
 async function firebaseInit() {
     const firebaseConfig = {
         apiKey: "AIzaSyD-6DKb5XdjbQgAPFTYNqrUUSLCVHUfIrE",
@@ -12,25 +14,22 @@ async function firebaseInit() {
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
     
-    const db = firebase.firestore();
-    const members = await getMembers(db);
-    addMembers(members.data());
+    db = firebase.firestore();
+    
+    addMembers();
+    addAllEvents();
+    initSemSelector();
+
+
 }
 
 
 
-async function getMembers(db) {
-    var members = [];
-    let querySnapshot = await db.collection("team").get();
-    querySnapshot.forEach((doc) => {
-        if(doc.id == "members")
-            members = doc;
-    });
-    return members;
-}
-
-function addMembers(members) {
-    const member_names = members.names;
+async function addMembers() {
+    let querySnapshot = await db.collection("team").doc("members").get();
+    if(!querySnapshot.exists) return; 
+    
+    const member_names = querySnapshot.data().names;
     const parent_section = document.getElementById("section-62ef6ed65edaa");
     for (let i = 0; i < member_names.length; i++) {
         const member_name = member_names[i];
@@ -44,6 +43,79 @@ function addMembers(members) {
     }
 }
 
+async function addAllEvents() {
+    const querySnapshot = await db.collection("events").get();
+    querySnapshot.forEach((doc) => {
+        const timeframe = doc.id;
+        
+
+        //TBD: Add event details
+        const events = doc.data().details;
+        for (let i = 0; i < events.length; i++) {
+            const event = events[i];
+            const event_date = event.time.toDate();
+
+            const event_outer_div = document.createElement("div");
+            event_outer_div.setAttribute("data-unique-id", makeid(13));
+            event_outer_div.className = "event clearfix box-primary preview-highlighter";
+
+
+            const event_details_div = document.createElement("div");
+            event_details_div.className = "event-details";
+            
+            const event_title_div = document.createElement("div");
+            event_title_div.className = "event-title";
+            event_title_div.innerHTML = `<h2>${event.name}</h2>`;
+
+            const event_meta = document.createElement("ul");
+            event_meta.className = "event-meta clearfix";
+            
+            const li_date = document.createElement("li");
+            const li_location = document.createElement("li");
+            let li_slides = document.createElement("li");
+            let li_recording = document.createElement("li");
+            let li_survey = document.createElement("li");
+
+            li_date.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="clock-o" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2');" data-ie11-classes="">&nbsp;</i>${getReadableDate(event_date)} @ ${getReadableTime(event_date)}`;
+            li_location.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="map-marker" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2');" data-ie11-classes="">&nbsp;</i>${event.location}`;
+            
+            if(!("slides" in event)) li_slides = null;
+            else li_slides.innerHTML = `<a href="${event.slides}"><i class="svg-m s123-icon-converter " data-icon-name="desktop" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Slides</b></a>`
+
+            if(!("recording" in event)) li_recording = null;
+            else li_recording.innerHTML = `<a href="${event.recording}"><i class="svg-m s123-icon-converter " data-icon-name="video-camera" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Recording</b></a>`;
+            
+            if(!("survey" in event)) li_survey = null;
+            else li_survey.innerHTML = `<a href="${event.survey}"><i class="svg-m s123-icon-converter " data-icon-name="list-alt" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Survey</b></a>`;
+
+            event_meta.appendChild(li_date);
+            event_meta.appendChild(li_location);
+            if(li_slides) event_meta.appendChild(li_slides);
+            if(li_recording) event_meta.appendChild(li_recording);
+            if(li_survey) event_meta.appendChild(li_survey);
+
+            event_details_div.appendChild(event_title_div);
+            event_details_div.appendChild(event_meta);
+            event_outer_div.appendChild(event_details_div);
+            document.getElementById("upcoming-events").appendChild(event_outer_div);
+
+        }
+
+        
+    });
+}
+
+async function initSemSelector() {
+    const selector = document.getElementById("event-sem-selector");
+    const querySnapshot = await db.collection("events").get();
+    querySnapshot.forEach((doc) => {
+        const option = document.createElement("option");
+        option.value = doc.id;
+        option.innerText = doc.id;
+        selector.appendChild(option);
+    });
+}
+
 function makeid(length) {
     let result = '';
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -55,3 +127,30 @@ function makeid(length) {
     }
     return result;
 }
+
+
+function getReadableDate(date) {
+    const monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+      ];
+    
+      const day = date.getDate();
+      const monthIndex = date.getMonth();
+      const year = date.getFullYear();
+    
+      return `${day} ${monthNames[monthIndex]} ${year}`;
+}
+
+function getReadableTime(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+  }
