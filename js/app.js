56d4dba340f30770bdb00f39ba04b96407bec253
Sid Srinivas <siddharths2710@yahoo.com>
2023-03-14 23:21:54 -0600

    Filter events by Semester

--- js/app.js
+++ js/app.js
@@ -17,9 +17,8 @@ async function firebaseInit() {
     db = firebase.firestore();
     
     addMembers();
-    addAllEvents();
     initSemSelector();
-
+    await addAllEvents();
 
 }
 
@@ -46,62 +45,8 @@ async function addMembers() {
 async function addAllEvents() {
     const querySnapshot = await db.collection("events").get();
     querySnapshot.forEach((doc) => {
-        const timeframe = doc.id;
-        
-
-        //TBD: Add event details
         const events = doc.data().details;
-        for (let i = 0; i < events.length; i++) {
-            const event = events[i];
-            const event_date = event.time.toDate();
-
-            const event_outer_div = document.createElement("div");
-            event_outer_div.setAttribute("data-unique-id", makeid(13));
-            event_outer_div.className = "event clearfix box-primary preview-highlighter";
-
-
-            const event_details_div = document.createElement("div");
-            event_details_div.className = "event-details";
-            
-            const event_title_div = document.createElement("div");
-            event_title_div.className = "event-title";
-            event_title_div.innerHTML = `<h2>${event.name}</h2>`;
-
-            const event_meta = document.createElement("ul");
-            event_meta.className = "event-meta clearfix";
-            
-            const li_date = document.createElement("li");
-            const li_location = document.createElement("li");
-            let li_slides = document.createElement("li");
-            let li_recording = document.createElement("li");
-            let li_survey = document.createElement("li");
-
-            li_date.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="clock-o" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2');" data-ie11-classes="">&nbsp;</i>${getReadableDate(event_date)} @ ${getReadableTime(event_date)}`;
-            li_location.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="map-marker" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2');" data-ie11-classes="">&nbsp;</i>${event.location}`;
-            
-            if(!("slides" in event)) li_slides = null;
-            else li_slides.innerHTML = `<a href="${event.slides}"><i class="svg-m s123-icon-converter " data-icon-name="desktop" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Slides</b></a>`
-
-            if(!("recording" in event)) li_recording = null;
-            else li_recording.innerHTML = `<a href="${event.recording}"><i class="svg-m s123-icon-converter " data-icon-name="video-camera" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Recording</b></a>`;
-            
-            if(!("survey" in event)) li_survey = null;
-            else li_survey.innerHTML = `<a href="${event.survey}"><i class="svg-m s123-icon-converter " data-icon-name="list-alt" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Survey</b></a>`;
-
-            event_meta.appendChild(li_date);
-            event_meta.appendChild(li_location);
-            if(li_slides) event_meta.appendChild(li_slides);
-            if(li_recording) event_meta.appendChild(li_recording);
-            if(li_survey) event_meta.appendChild(li_survey);
-
-            event_details_div.appendChild(event_title_div);
-            event_details_div.appendChild(event_meta);
-            event_outer_div.appendChild(event_details_div);
-            document.getElementById("upcoming-events").appendChild(event_outer_div);
-
-        }
-
-        
+        renderEvents(events);
     });
 }
 
@@ -154,3 +99,72 @@ function getReadableTime(date) {
     var strTime = hours + ':' + minutes + ' ' + ampm;
     return strTime;
   }
+
+async function filterEventsBySemester(semester) {
+    document.getElementById("upcoming-events").innerHTML = "";
+    if(semester === "all") {
+        await addAllEvents();
+        return;
+    }
+    await addEventsBySemester(semester);
+}
+
+async function addEventsBySemester(semester) {
+    const querySnapshot = await db.collection("events").doc(semester).get();
+    if(!querySnapshot.exists) return;
+    const events = querySnapshot.data().details;
+    renderEvents(events);
+}
+
+function renderEvents(events) {
+
+    for (let i = 0; i < events.length; i++) {
+        const event = events[i];
+        const event_date = event.time.toDate();
+
+        const event_outer_div = document.createElement("div");
+        event_outer_div.setAttribute("data-unique-id", makeid(13));
+        event_outer_div.className = "event clearfix box-primary preview-highlighter";
+
+
+        const event_details_div = document.createElement("div");
+        event_details_div.className = "event-details";
+        
+        const event_title_div = document.createElement("div");
+        event_title_div.className = "event-title";
+        event_title_div.innerHTML = `<h2>${event.name}</h2>`;
+
+        const event_meta = document.createElement("ul");
+        event_meta.className = "event-meta clearfix";
+        
+        const li_date = document.createElement("li");
+        const li_location = document.createElement("li");
+        let li_slides = document.createElement("li");
+        let li_recording = document.createElement("li");
+        let li_survey = document.createElement("li");
+
+        li_date.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="clock-o" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/clock-o.svg?v=2');" data-ie11-classes="">&nbsp;</i>${getReadableDate(event_date)} @ ${getReadableTime(event_date)}`;
+        li_location.innerHTML = `<i class="svg-m s123-icon-converter " data-icon-name="map-marker" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/map-marker.svg?v=2');" data-ie11-classes="">&nbsp;</i>${event.location}`;
+        
+        if(!("slides" in event)) li_slides = null;
+        else li_slides.innerHTML = `<a href="${event.slides}"><i class="svg-m s123-icon-converter " data-icon-name="desktop" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/desktop.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Slides</b></a>`
+
+        if(!("recording" in event)) li_recording = null;
+        else li_recording.innerHTML = `<a href="${event.recording}"><i class="svg-m s123-icon-converter " data-icon-name="video-camera" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/video-camera.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Recording</b></a>`;
+        
+        if(!("survey" in event)) li_survey = null;
+        else li_survey.innerHTML = `<a href="${event.survey}"><i class="svg-m s123-icon-converter " data-icon-name="list-alt" style=" mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2'); -webkit-mask: url('https://static1.s123-cdn-static-a.com/ready_uploads/svg/list-alt.svg?v=2');" data-ie11-classes="">&nbsp;</i><b>Survey</b></a>`;
+
+        event_meta.appendChild(li_date);
+        event_meta.appendChild(li_location);
+        if(li_slides) event_meta.appendChild(li_slides);
+        if(li_recording) event_meta.appendChild(li_recording);
+        if(li_survey) event_meta.appendChild(li_survey);
+
+        event_details_div.appendChild(event_title_div);
+        event_details_div.appendChild(event_meta);
+        event_outer_div.appendChild(event_details_div);
+        document.getElementById("upcoming-events").appendChild(event_outer_div);
+
+    }
+}
