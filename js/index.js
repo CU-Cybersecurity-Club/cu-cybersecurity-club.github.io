a0d6d9148e8bd4901740da8b77bf487434899134
Kevin <kevinshu.colorado@gmail.com>
2026-01-21 17:12:45 -0700

    Spring update
    Also abbreviated month names in event calendar so they fit a bit better

--- js/index.js
+++ js/index.js
@@ -115,7 +115,7 @@ async function addMembers() {
 }
 
 function getReadableDate(date) {
-	const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	const day = date.getDate();
 	const monthIndex = date.getMonth();
 	const year = date.getFullYear();
